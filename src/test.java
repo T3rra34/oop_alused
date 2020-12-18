774e329592438d351e0b8fdb75ea410a8455ad17
Henri Kazbekov <kazbekov.henri@khk.ee>
2020-12-18 23:30:26 +0200

    Teade väljastamine java abil

--- src/test.java
+++ src/test.java
@@ -1,7 +1,10 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Tere, java programm");
-        System.out.println("See on java IDE katse");
+        System.out.println("Täna õppime:");
+        System.out.println("1. JDK installimine ja seadistamine");
+        System.out.println("2. IDE installimine ja seadistamine");
+        System.out.println("3. IDE kasutamine");
+        System.out.println("4. git VCS kasutamine");
     }
 }
 
