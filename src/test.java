25f5428aa729e0b48dc3818537ea25cdc8ba6129
Henri Kazbekov <kazbekov.henri@khk.ee>
2020-12-18 23:24:55 +0200

    Esimene java programm on loodud

---
+++ src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Tere, java programm");
+        System.out.println("See on java IDE katse");
+    }
+}
+
