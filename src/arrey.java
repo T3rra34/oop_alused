b3621f00d9bb1d2ef7a63725fac66e1fdb3719c4
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-01-11 16:38:55 +0200

    massiivid [] 123 ja {} Ülesanne 5.4B

--- src/arrey.java
+++ src/arrey.java
@@ -1,15 +1,21 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
+/* Ülesanne 5.4A
+ * Henri Kazbekov
+ * */
 
 public class arrey {
     public static void main(String[] args) {
-        int[] vastuvoetud = {2803, 2626, 2359, 1927, 2236, 2281, 2394, 2484, 2468};
-        Scanner sisend = new Scanner(System.in);
-        int aasta = sisend.nextInt();
-        int massiiviindex = aasta - 2011;
-        System.out.println("Vastuvõeti" + vastuvoetud[massiiviindex]);
-
+        int[] sisse = [3884, 2810, 3709, 2639, 4098, 3904, 15413, 14822, 17616, 17547];
+        int[] valja = [4658, 5294, 6214, 6321, 6740, 4637, 13003, 13792, 12358, 10476];
+        System.out.println(sisse[1] - valja[1]);
+        System.out.println(sisse[2] - valja[2]);
+        System.out.println(sisse[3] - valja[3]);
+        System.out.println(sisse[4] - valja[4]);
+        System.out.println(sisse[5] - valja[5]);
+        System.out.println(sisse[6] - valja[6]);
+        System.out.println(sisse[7] - valja[7]);
+        System.out.println(sisse[8] - valja[8]);
+        System.out.println(sisse[9] - valja[9]);
+        System.out.println(sisse[10] - valja[10]);
     }
-
 
 }
