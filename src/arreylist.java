488ff092d63a960d1a744cd36f20aa4c4df7dff8
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-01-15 13:17:56 +0200

    Ülesande algus

---
+++ src/arreylist.java
@@ -0,0 +1,14 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class arreylist {
+    private static String nimi;
+
+    public static void main(String[] args) {
+
+    }
+
+}
