18cd7ad5d5b3c6e8dbc4a669b351b46c56f91f0d
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-01-27 10:08:56 +0200

    algus

---
+++ src/inimene.java
@@ -0,0 +1,16 @@
+public class Inimene {
+    Inimene matti = new Inimene("Matti");
+    private double Pikkus;
+    Inimenejohn =new
+
+    Inimene("John");
+
+    matti.setPikkus(180);
+    matti.setMass(86);
+
+    john.setPikkus(175);
+    john.setMass(64);
+
+    System.out.println(matti.getNimi()+", kehamassiindeks: "+matti.kmi());
+    System.out.println(john.getNimi()+", kehamassiindeks: "+john.kmi());
+}
